68a939cb92eb56b7c4e12273a3c98e537d39b35d
Claudia <claudia.wick@students.unibe.ch>
2017-02-18 15:59:13 +0100

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,6 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
